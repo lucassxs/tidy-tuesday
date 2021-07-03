a4417af8f2b2796dbc0a2a8291f73aa4ee7a85b3
Lucas Stefano <lucassxs.sp@gmail.com>
2021-07-03 18:06:09 -0300

    📝 update section headers

---
+++ 2021/17_netflix/17_netflix.R
@@ -0,0 +1,16 @@
+#date: July 03, 2021
+
+#install packages
+install.packages('tidyverse')
+install.packages('ggrepel')
+install.packages('extrafont')
+install.packages('Cairo')
+install.packages('gganimate')
+
+#load packages
+library(tidyverse)
+library(ggrepel)
+library(extrafont)
+library(Cairo)
+library(gganimate)
+

01dc949ed19105eb1e524097e1371903930112d9
Lucas Stefano <lucassxs.sp@gmail.com>
2021-07-13 14:09:16 -0300

    📝 add packages

---
+++ 2021/29_scoobydoo-episodes/29_scoobydoo-episodes.R
@@ -0,0 +1,15 @@
+#date July 13, 2021
+
+#install packages
+install.packages('tidyverse')
+install.packages('ggrepel')
+install.packages('extrafont')
+install.packages('Cairo')
+install.packages('gganimate')
+
+#load packages
+library(tidyverse)
+library(ggrepel)
+library(extrafont)
+library(Cairo)
+library(gganimate)

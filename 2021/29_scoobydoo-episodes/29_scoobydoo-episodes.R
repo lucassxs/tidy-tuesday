7bafbe45646c24b877458d9cc4690466629bd47b
Lucas Stefano <lucassxs.sp@gmail.com>
2021-07-13 14:11:15 -0300

    🖥️ read database csv

--- 2021/29_scoobydoo-episodes/29_scoobydoo-episodes.R
+++ 2021/29_scoobydoo-episodes/29_scoobydoo-episodes.R
@@ -1,15 +1,19 @@
 #date July 13, 2021
 
-#install packages
+# install packages
 install.packages('tidyverse')
 install.packages('ggrepel')
 install.packages('extrafont')
 install.packages('Cairo')
 install.packages('gganimate')
 
-#load packages
+# load packages
 library(tidyverse)
 library(ggrepel)
 library(extrafont)
 library(Cairo)
 library(gganimate)
+
+# read database  
+
+scoobydoo <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2021/2021-07-13/scoobydoo.csv')

987402429318a29ad53baf949b5af5bbe5d74f54
Lucas Stefano <lucassxs.sp@gmail.com>
2021-07-22 21:51:43 -0300

    📦  Load Library

---
+++ 2021/30_us-droughts/30_us-droughts.R
@@ -0,0 +1,10 @@
+# Install Packages
+install.packages('tidyverse')
+install.packages('ggtext')
+install.packages('extrafont')
+
+
+# Load Packages
+library(tidyverse)
+library(ggtext)
+library(extrafont)

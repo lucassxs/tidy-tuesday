9bb486fadca044cdb183ae93034ce1d80975a526
Lucas Stefano <lucassxs.sp@gmail.com>
2021-07-26 21:14:26 -0300

    📝 add data

--- 2021/31_olympics/31_olympics.R
+++ 2021/31_olympics/31_olympics.R
@@ -6,3 +6,9 @@ library(usmap)
 library(RColorBrewer)
 library(extrafont)
 fonttable <- fonttable()
+
+loadfonts(device = "win", quiet = TRUE) ## to load the font
+
+# Import the data
+
+olympics <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2021/2021-07-27/olympics.csv')

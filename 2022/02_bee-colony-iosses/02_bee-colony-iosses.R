53f02251071217d6c880c4015795e18a613dcc6c
Lucas Stefano Xavier de Sousa <lucassxs@archlucas.lucas>
2022-01-16 18:17:21 -0300

    🐝 week 02: bee colony

--- 2022/02_bee-colony-iosses/02_bee-colony-iosses.R
+++ 2022/02_bee-colony-iosses/02_bee-colony-iosses.R
@@ -2,3 +2,62 @@
 # Theme: Bee Colonies
 # Date: Jan 16 2022
 # Author: Lucas Stefano
+
+# Load packages
+library(tidyverse)
+library(showtext)
+library(scales)
+
+# Get the data
+tuesdata <- tidytuesdayR::tt_load('2022-01-11')
+colony <- tuesdata$colony
+
+# Get font
+font_add_google('IBM Plex Sans', family = 'ibm')
+
+# Automatically use showtext to render text for future devices
+showtext_auto()
+showtext_opts(dpi = 300)
+
+# Work \o/
+colony %>%
+  filter(!is.na(colony_added),
+         !is.na(colony_lost)) %>%
+  group_by(year) %>%
+  summarise(colonies_added = sum(colony_added),
+            colonies_lost = sum(colony_lost)) %>%
+  mutate(fill_cond = case_when(year == 2015 | year == 2021 ~ 'highlight',
+                               TRUE ~ 'normal')) %>%
+  ggplot() + 
+  geom_col(aes(year, colonies_added, fill = fill_cond), show.legend = FALSE) +
+  scale_y_continuous(labels = label_comma()) +
+  scale_x_continuous(breaks = breaks_pretty(n = 7)) +
+  scale_fill_manual(values = c('#deb841', '#6d6a75')) +
+  labs(title = 'Number of Bee Colonies in the US from 2015 to 2021',
+       subtitle = 'teste',
+       y = 'Colonies Added',
+       x = NULL,
+       caption = 'By Lucas Stefano | Departament Statistics UEM') +
+  theme(axis.text = element_text(family = 'ibm',
+                                 size = 9),
+        axis.title = element_text(family = 'ibm',
+                                  size = 9),
+        plot.title = element_text(family = 'ibm',
+                                  size = 12),
+        plot.subtitle = element_text(family = 'ibm',
+                                     size = 9),
+        plot.caption = ggtext::element_markdown(family = 'ibm',
+                                                size = 6),
+        panel.background = element_rect('#F5F5F5'),
+        plot.background = element_rect('#F5F5F5'),
+        panel.grid = element_blank(),
+        axis.ticks.y = element_blank(),
+        plot.title.position = 'plot',
+        plot.caption.position = 'plot',
+        plot.margin = margin(25, 15, 10, 15))
+
+# save output
+ggsave(paste0("02bee_colony", format(Sys.time(), "%d%m%Y"), ".png"),
+       dpi = 320,
+       width = 12,
+       height =  7)

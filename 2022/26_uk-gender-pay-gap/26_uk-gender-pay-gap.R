4678359ddcf423762691fe51df7ebe74b776753c
Lucas Stefano <lucassxs.sp@gmail.com>
2022-06-28 21:06:20 -0300

    📅 add 26 week

---
+++ 2022/26_uk-gender-pay-gap/26_uk-gender-pay-gap.R
@@ -0,0 +1,16 @@
+# Week 26
+# Theme: UK Gender pay gap
+# Date: Jan 16 2022
+# Author: Lucas Stefano
+
+# Load packages
+library(tidyverse)
+library(showtext)
+
+
+# Get the data
+tuesdata <- tidytuesdayR::tt_load('2022-06-28')
+paygap <- tuesdata$paygap
+
+
+
